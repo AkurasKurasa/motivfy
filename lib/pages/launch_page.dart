ea9c84ff00fb407027fedc8fdf818e15b0fdecc1
Akuras Kurasa <paulandreiscalma@gmail.com>
2025-07-08 19:10:45 +0800

    updated launch

--- lib/pages/launch_page.dart
+++ lib/pages/launch_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:google_fonts/google_fonts.dart';
-// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LaunchPage extends StatefulWidget {
   const LaunchPage({super.key});
@@ -11,213 +9,195 @@ class LaunchPage extends StatefulWidget {
   State<LaunchPage> createState() => _LaunchPageState();
 }
 
-class _LaunchPageState extends State<LaunchPage> {
+class _LaunchPageState extends State<LaunchPage> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _offsetAnimation;
 
-  @override 
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 700),
+      vsync: this,
+    );
+    _offsetAnimation = Tween<Offset>(
+      begin: const Offset(0, 0.2),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
 
-          Container( 
-            margin: EdgeInsets.fromLTRB(32, 56, 32, 0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    SizedBox(width: 4),
-                    Text("Hello, John!", style: GoogleFonts.inter(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 16, letterSpacing: -0.1) ),
-                    SizedBox(width: 16),
-                    SizedBox(height: 24, width: 24, child: SvgPicture.asset('envelope.svg') )
-                  ],
-                ),
-                SizedBox( height: 32, width: 32, child: SvgPicture.asset('assets/double-arrow.svg') )
-              ],
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget buildFeatureCard(String asset, String label, double iconSize) {
+    return _HoverCard(
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(width: 1.5, color: const Color(0xFF505050)),
+          borderRadius: BorderRadius.circular(9),
+        ),
+        width: 125,
+        height: 125,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: iconSize, width: iconSize, child: SvgPicture.asset(asset)),
+            const SizedBox(height: 8),
+            Text(
+              label,
+              style: GoogleFonts.quicksand(
+                color: const Color(0xFF878787),
+                fontWeight: FontWeight.w400,
+                height: 1.0,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-
-          SizedBox(height: 24),
-
-          Opacity(
-            opacity: 0.5,
-            child: Container(
-              margin: EdgeInsets.fromLTRB(32, 0, 32, 0),
-              decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [Color(0x73FFFFFF), Color(0x00FFFFFF)], stops: [0, 1.0], begin: Alignment(1, -1), end: Alignment(-1, 1) ),
-                borderRadius: BorderRadius.circular(16)
-              ), 
-              width: double.infinity,
-              height: 175
-            )
-          ),
-
-          SizedBox(height: 56),
-
-          Container(
-            margin: EdgeInsets.fromLTRB(32, 0, 32, 0),
-            decoration: BoxDecoration(
-              // border: Border.all(
-              //   color: Colors.white
-              // )
-            ), 
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-
-                Row(
-                  children: [
-                    Text("Discover", style: GoogleFonts.quicksand(color: Colors.white, fontWeight: FontWeight.w700, fontSize: 32))
-                  ],
-                ),
-
-                SizedBox(height: 24),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  spacing: 8.0,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 1.5,
-                          color: Color(0xFF505050)
-                        ),
-                        borderRadius: BorderRadius.circular(9)
-                      ),
-                      width: 125,
-                      height: 125,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(height: 32, width: 32, child: SvgPicture.asset('timer.svg') ),
-                          SizedBox(height: 8),
-                          Text('Pomodoro\nTimer', style: GoogleFonts.quicksand(color: Color(0xFF878787), fontWeight: FontWeight.w400, height: 1.0), textAlign: TextAlign.center)
-                        ],
-                      ),
-                    ),
-
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 1.5,
-                          color: Color(0xFF505050)
-                        ),
-                        borderRadius: BorderRadius.circular(9)
-                      ),
-                      width: 125,
-                      height: 125,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(height: 40, width: 40, child: SvgPicture.asset('analyze.svg') ),
-                          SizedBox(height: 8),
-                          Text('Procrastination\nAnalysis', style: GoogleFonts.quicksand(color: Color(0xFF878787), fontWeight: FontWeight.w400, height: 1.0), textAlign: TextAlign.center)
-                        ],
-                      ),
-                    ),
-
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 1.5,
-                          color: Color(0xFF505050)
-                        ),
-                        borderRadius: BorderRadius.circular(9)
-                      ),
-                      width: 125,
-                      height: 125,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(height: 40, width: 40, child: SvgPicture.asset('block.svg') ),
-                          SizedBox(height: 8),
-                          Text('Block\nList', style: GoogleFonts.quicksand(color: Color(0xFF878787), fontWeight: FontWeight.w400, height: 1.0), textAlign: TextAlign.center)
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-
-                SizedBox(height: 8),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  spacing: 8.0,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 1.5,
-                          color: Color(0xFF505050)
-                        ),
-                        borderRadius: BorderRadius.circular(9)
-                      ),
-                      width: 125,
-                      height: 125,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(height: 32, width: 32, child: SvgPicture.asset('timer.svg') ),
-                          SizedBox(height: 8),
-                          Text('Done\nList', style: GoogleFonts.quicksand(color: Color(0xFF878787), fontWeight: FontWeight.w400, height: 1.0), textAlign: TextAlign.center)
-                        ],
-                      ),
-                    ),
-
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 1.5,
-                          color: Color(0xFF505050)
-                        ),
-                        borderRadius: BorderRadius.circular(9)
-                      ),
-                      width: 125,
-                      height: 125,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(height: 40, width: 40, child: SvgPicture.asset('analyze.svg') ),
-                          SizedBox(height: 8),
-                          Text('Note\nFlow', style: GoogleFonts.quicksand(color: Color(0xFF878787), fontWeight: FontWeight.w400, height: 1.0), textAlign: TextAlign.center)
-                        ],
-                      ),
-                    ),
-
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 1.5,
-                          color: Color(0xFF505050)
-                        ),
-                        borderRadius: BorderRadius.circular(9)
-                      ),
-                      width: 125,
-                      height: 125,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(height: 40, width: 40, child: SvgPicture.asset('block.svg') ),
-                          SizedBox(height: 8),
-                          Text('Productivity\nAssistant', style: GoogleFonts.quicksand(color: Color(0xFF878787), fontWeight: FontWeight.w400, height: 1.0), textAlign: TextAlign.center)
-                        ],
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
+          ],
+        ),
       ),
-      backgroundColor: Color(0xFF181818),
     );
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF181818),
+      body: SlideTransition(
+        position: _offsetAnimation,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.fromLTRB(32, 56, 32, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      const SizedBox(width: 4),
+                      Text(
+                        "Hello, John!",
+                        style: GoogleFonts.inter(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                          letterSpacing: -0.1,
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      SizedBox(height: 24, width: 24, child: SvgPicture.asset('envelope.svg')),
+                    ],
+                  ),
+                  SizedBox(height: 32, width: 32, child: SvgPicture.asset('assets/double-arrow.svg')),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 24),
+
+            Opacity(
+              opacity: 0.5,
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    colors: [Color(0x73FFFFFF), Color(0x00FFFFFF)],
+                    stops: [0, 1.0],
+                    begin: Alignment(1, -1),
+                    end: Alignment(-1, 1),
+                  ),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                width: double.infinity,
+                height: 175,
+              ),
+            ),
+
+            const SizedBox(height: 56),
+
+            Container(
+              margin: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Discover",
+                        style: GoogleFonts.quicksand(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 32,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(height: 24),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      buildFeatureCard('timer.svg', 'Pomodoro\nTimer', 32),
+                      const SizedBox(width: 8),
+                      buildFeatureCard('analyze.svg', 'Procrastination\nAnalysis', 40),
+                      const SizedBox(width: 8),
+                      buildFeatureCard('block.svg', 'Block\nList', 40),
+                    ],
+                  ),
+
+                  const SizedBox(height: 8),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      buildFeatureCard('timer.svg', 'Done\nList', 32),
+                      const SizedBox(width: 8),
+                      buildFeatureCard('analyze.svg', 'Note\nFlow', 40),
+                      const SizedBox(width: 8),
+                      buildFeatureCard('block.svg', 'Productivity\nAssistant', 40),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _HoverCard extends StatefulWidget {
+  final Widget child;
+  const _HoverCard({required this.child});
+
+  @override
+  State<_HoverCard> createState() => _HoverCardState();
+}
+
+class _HoverCardState extends State<_HoverCard> {
+  bool _hovering = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) => setState(() => _hovering = true),
+      onExit: (_) => setState(() => _hovering = false),
+      child: AnimatedScale(
+        scale: _hovering ? 1.05 : 1.0,
+        duration: const Duration(milliseconds: 150),
+        child: widget.child,
+      ),
+    );
+  }
 }
--- lib/pages/notification_page.dart
+++ lib/pages/notification_page.dart
@@ -9,7 +9,77 @@ class NotificationPage extends StatefulWidget {
   State<NotificationPage> createState() => _NotificationPageState();
 }
 
-class _NotificationPageState extends State<NotificationPage> {
+class _NotificationPageState extends State<NotificationPage> with TickerProviderStateMixin {
+  late final List<AnimationController> _controllers = [];
+  late final List<Animation<Offset>> _slideAnimations = [];
+  late final List<Animation<double>> _fadeAnimations = [];
+
+  final int todayCount = 2;
+  final int yesterdayCount = 6;
+
+  @override
+  void initState() {
+    super.initState();
+
+    final totalItems = todayCount + yesterdayCount;
+
+    for (int i = 0; i < totalItems; i++) {
+      final controller = AnimationController(
+        duration: const Duration(milliseconds: 500),
+        vsync: this,
+      );
+
+      final slideAnimation = Tween<Offset>(
+        begin: const Offset(0, 0.15),
+        end: Offset.zero,
+      ).animate(CurvedAnimation(
+        parent: controller,
+        curve: Curves.easeOut,
+      ));
+
+      final fadeAnimation = Tween<double>(
+        begin: 0.0,
+        end: 1.0,
+      ).animate(CurvedAnimation(
+        parent: controller,
+        curve: Curves.easeIn,
+      ));
+
+      _controllers.add(controller);
+      _slideAnimations.add(slideAnimation);
+      _fadeAnimations.add(fadeAnimation);
+
+      // Staggered animation start
+      Future.delayed(Duration(milliseconds: 100 * i), () {
+        if (mounted) _controllers[i].forward();
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    for (final controller in _controllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  Widget _buildSectionHeader(String title) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: GoogleFonts.quicksand(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +89,6 @@ class _NotificationPageState extends State<NotificationPage> {
           Container(
             padding: const EdgeInsets.fromLTRB(32, 40, 32, 0),
             child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 SizedBox(
                   height: 26,
@@ -33,8 +102,22 @@ class _NotificationPageState extends State<NotificationPage> {
                     color: Colors.white,
                     fontSize: 22,
                     fontWeight: FontWeight.w700,
+                  )
+                ),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                    "Mark All As Read",
+                    style: GoogleFonts.quicksand(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                    )
                   ),
+                  )
                 )
+                
               ],
             ),
           ),
@@ -45,16 +128,23 @@ class _NotificationPageState extends State<NotificationPage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  _buildSectionHeader("Today", showMarkAll: true),
+                  _buildSectionHeader("Today"),
                   const SizedBox(height: 24),
-                  _NotificationItem(),
-                  const SizedBox(height: 32),
-                  _NotificationItem(),
-                  const SizedBox(height: 48),
+                  for (int i = 0; i < todayCount; i++) ...[
+                    _NotificationItem(
+                      slideAnimation: _slideAnimations[i],
+                      fadeAnimation: _fadeAnimations[i],
+                    ),
+                    const SizedBox(height: 32),
+                  ],
+                  const SizedBox(height: 16),
                   _buildSectionHeader("Yesterday"),
                   const SizedBox(height: 24),
-                  for (int i = 0; i < 6; i++) ...[
-                    _NotificationItem(),
+                  for (int i = todayCount; i < todayCount + yesterdayCount; i++) ...[
+                    _NotificationItem(
+                      slideAnimation: _slideAnimations[i],
+                      fadeAnimation: _fadeAnimations[i],
+                    ),
                     const SizedBox(height: 32),
                   ],
                 ],
@@ -65,90 +155,77 @@ class _NotificationPageState extends State<NotificationPage> {
       ),
     );
   }
-
-  Widget _buildSectionHeader(String title, {bool showMarkAll = false}) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          title,
-          style: GoogleFonts.quicksand(
-            color: Colors.white,
-            fontSize: 16,
-            fontWeight: FontWeight.w700,
-          ),
-        ),
-        if (showMarkAll)
-          Text(
-            "Mark All As Read",
-            style: GoogleFonts.quicksand(
-              color: Colors.white,
-              fontSize: 12,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-      ],
-    );
-  }
 }
 
 class _NotificationItem extends StatelessWidget {
-  const _NotificationItem();
+  final Animation<Offset> slideAnimation;
+  final Animation<double> fadeAnimation;
+
+  const _NotificationItem({
+    required this.slideAnimation,
+    required this.fadeAnimation,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          height: 72,
-          width: 72,
-          decoration: BoxDecoration(
-            color: const Color(0xFF383838),
-            borderRadius: BorderRadius.circular(100),
-          ),
-        ),
-        const SizedBox(width: 24),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt elit, sed do eiusmod tempor incididunt",
-                style: GoogleFonts.quicksand(
-                  color: const Color(0xFF888888),
-                  fontSize: 12,
-                  height: 1.45,
-                ),
-                textAlign: TextAlign.justify,
-                maxLines: 3,
-                overflow: TextOverflow.ellipsis,
+    return SlideTransition(
+      position: slideAnimation,
+      child: FadeTransition(
+        opacity: fadeAnimation,
+        child: Row(
+          children: [
+            Container(
+              height: 72,
+              width: 72,
+              decoration: BoxDecoration(
+                color: const Color(0xFF383838),
+                borderRadius: BorderRadius.circular(100),
               ),
-              const SizedBox(height: 8),
-              Row(
+            ),
+            const SizedBox(width: 24),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    width: 6,
-                    height: 6,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFFE83E3E),
-                      borderRadius: BorderRadius.circular(100),
-                    ),
-                  ),
-                  const SizedBox(width: 8),
                   Text(
-                    "1 minute ago",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt elit, sed do eiusmod tempor incididunt",
                     style: GoogleFonts.quicksand(
-                      color: Colors.white,
-                      fontSize: 10,
-                      fontWeight: FontWeight.w700,
+                      color: const Color(0xFF888888),
+                      fontSize: 12,
+                      height: 1.45,
                     ),
-                  )
+                    textAlign: TextAlign.justify,
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      Container(
+                        width: 6,
+                        height: 6,
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFE83E3E),
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        "1 minute ago",
+                        style: GoogleFonts.quicksand(
+                          color: Colors.white,
+                          fontSize: 10,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
-            ],
-          ),
-        )
-      ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
